7ae0a9848a6d339452f92506cd7e8ea9f3584e1c
Philip Woods <elzairthesorcerer@gmail.com>
2014-08-20 16:26:53 -0400

    Configured settings for closure

--- init.el
+++ init.el
@@ -1,4 +1,7 @@
-; set package list
+;;; package --- Elzair's Emacs init file
+;;; Commentary:
+;;; This is my personal start-up file for Emacs.
+;;; Code:
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
@@ -8,7 +11,7 @@
 (add-to-list 'load-path "~/.emacs.d/scripts")
 
 ; list of installed packages
-(setq package-list '(auto-complete ac-slime dash evil flycheck goto-chg load-theme-buffer-local nav popup quack rainbow-delimiters slime slime-scratch smart-tab smartparens solarized-theme tern undo-tree))
+(setq package-list '(ac-slime auto-complete cider clojure-mode clojure-test-mode clojurescript-mode dash evil evil-leader flycheck goto-chg load-theme-buffer-local nav popup quack rainbow-delimiters slime slime-scratch smart-tab smartparens solarized-theme tern undo-tree))
 
 ; initialize packages
 (package-initialize)
@@ -26,6 +29,7 @@
 (require 'ert)
 (require 'follow-mouse)
 (require 'evil)
+(require 'evil-leader)
 (require 'key-chord)
 (require 'frame-cmds)
 (require 'nav)
@@ -38,6 +42,7 @@
 
 ; enable/disable certain features
 (scroll-bar-mode -1)              ; disable scroll bar
+(horizontal-scroll-bar-mode -1)   ; disable bottom scroll bar
 (tool-bar-mode -1)                ; disable tool bar 
 (setq initial-scratch-buffer nil)
 (turn-on-follow-mouse)            ; turn on focus follows mouse
@@ -56,9 +61,8 @@
   (setq inferior-lisp-program "/usr/bin/sbcl")))
 (require 'slime-autoloads)
 (slime-setup '(slime-fancy slime-asdf slime-banner))
-; set-up SLIME autocomplete
 (defun my-slime-mode-hook ()
-  (set-up-slime-ac)
+  (set-up-slime-ac) ; set-up SLIME autocomplete
   (rainbow-delimiters-mode))
 (eval-after-load "auto-complete"
   '(add-to-list 'ac-modes 'slime-repl-mode))
@@ -73,19 +77,29 @@
 (defun my-inferior-scheme-mode-hook ()
   (require 'quack)
   (setq quack-fontify-style 'emacs)
-  (rainbow-delimiters-mode)
-)
+  (rainbow-delimiters-mode))
 
 ; configure scheme files
 (defun my-scheme-mode-hook ()
   (linum-mode)
-  (rainbow-delimiters-mode)
-)
+  (rainbow-delimiters-mode))
 
 ; configure javascript files
 (defun my-js2-mode-hook ()
   (linum-mode)
   (tern-mode t))
+
+(defun my-emacs-lisp-mode-hook ()
+  (linum-mode)
+  (rainbow-delimiters-mode))
+
+(defun my-clojure-mode-hook ()
+  (linum-mode)
+  (rainbow-delimiters-mode))
+
+(defun my-clojurescript-mode-hook ()
+  (linum-mode)
+  (rainbow-delimiters-mode))
 
 (add-hook 'slime-mode-hook 'my-slime-mode-hook)
 (add-hook 'slime-repl-mode-hook 'my-slime-mode-hook)
@@ -93,6 +107,9 @@
 (add-hook 'inferior-scheme-mode-hook 'my-inferior-scheme-mode-hook)
 (add-hook 'scheme-mode-hook 'my-scheme-mode-hook)
 (add-hook 'js2-mode-hook 'my-js2-mode-hook)
+(add-hook 'emacs-lisp-mode-hook 'my-emacs-lisp-mode-hook)
+(add-hook 'clojure-mode-hook 'my-clojure-mode-hook)
+(add-hook 'clojurescript-mode-hook 'my-clojurescript-mode-hook)
 
 (require 'smartparens-config)
 (smartparens-global-mode t)
@@ -110,10 +127,16 @@
 (setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
 
 ; enable and configure vi emulation
+(global-evil-leader-mode)
 (evil-mode 1)
 (setq key-chord-two-keys-delay 0.5)
+; vim-style keybindings
 (key-chord-define evil-insert-state-map "ii" 'evil-normal-state)
+(evil-leader/set-leader "-")
+(evil-leader/set-key 
+  "e v" (lambda () (interactive) (evil-window-vnew 80 "~/.emacs.d/init.el")))
 (key-chord-mode 1)
+(evil-ex-define-cmd "er" 'eval-region)
 
 ; set default font and theme
 (set-default-font "InconsolataLGC-12")
@@ -132,3 +155,6 @@
 (cond
  ((string-match "darwin" system-configuration)
   (toggle-frame-fullscreen)))
+
+(provide 'init)
+;;; init ends here
