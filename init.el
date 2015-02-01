f2780c7994c2de55e6a10e907515005183fbb03b
Philip Woods <elzairthesorcerer@gmail.com>
2015-02-01 15:27:56 -0500

    Updated emacs config

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
-(defvar package-list '(cider clojure-mode clojure-test-mode clojurescript-mode company company-c-headers company-irony company-tern dash dockerfile-mode emmet-mode evil evil-leader flycheck flycheck-pos-tip geiser goto-chg haskell-mode julia-mode load-theme-buffer-local magit markdown-mode neotree org org-present php-extras popup punctuality-logger quack rainbow-delimiters sly sly-company solarized-theme tern toml-mode undo-tree web-mode yaml-mode yasnippet)
+(defvar package-list '(cider clojure-mode clojure-test-mode clojurescript-mode company company-c-headers company-irony company-tern dash dockerfile-mode emmet-mode evil evil-leader flycheck flycheck-pos-tip geiser goto-chg haskell-mode julia-mode load-theme-buffer-local magit markdown-mode neotree org org-present php-extras popup punctuality-logger quack rainbow-delimiters restclient sly sly-company solarized-theme tern toml-mode undo-tree web-mode yaml-mode yasnippet)
   "List of installed packages.")
 
 (package-initialize) ;; Initialize packages
