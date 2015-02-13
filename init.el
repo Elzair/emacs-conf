5f63b7daccfc8e9c76a25ef5f21a27d7c80e6cea
Philip Woods <elzairthesorcerer@gmail.com>
2015-02-13 09:12:14 -0500

    Updated emacs config

--- init.el
+++ init.el
@@ -31,3 +31,18 @@ and install the package."
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(android-mode-sdk-dir "/opt/android-sdk-linux")
+ '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
+ '(magit-use-overlays nil)
+ '(punctuality-logger-use-version-control t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
